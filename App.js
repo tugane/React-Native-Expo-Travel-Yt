3f99b04dce5cdbf0161a6c8994c7d084f514a4b4
Tugane <tugane@Tuganes-MacBook-Pro.local>
2022-06-09 10:57:40 +0200

    HomeScreen

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
-import TourDetailScreen from "./app/screens/TourDetailScreen";
-import TOURS from "./app/config/TOURS";
+import HomeScreen from "./app/screens/HomeScreen";
 
 export default function App() {
-  return <TourDetailScreen tour={TOURS[3]} />;
+  return <HomeScreen />;
 }
